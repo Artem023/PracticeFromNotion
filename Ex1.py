679821e19da1dc45046ee9636e450a80d7545fd8
Artem Neliubov <artemneliubov@yandex.ru>
2022-12-01 13:46:34 +0300

    Exercise 1 done

---
+++ Ex1.py
@@ -0,0 +1,6 @@
+num1 = int (input ("Enter first number: "))
+num2 = int (input ("Enter second number: "))
+
+if (num1 ** 2 == num2):
+    print("Number", num1, "is the square of number", num2)
+else: print ("Take another number")
